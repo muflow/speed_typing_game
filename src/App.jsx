0bb168b9103ec1fd0bed0a045cd85e2d028c3597
andrea ilieschi <andrea.ilieschi@gmail.com>
2024-05-06 14:46:43 +0200

    delivery mvp

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,60 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 export default function App() {
+  const STARTING_TIME = 5
+
   const [text, setText] = useState('');
+  const [countdown, setCountdown] = useState(STARTING_TIME);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
 
   function handleChange(e) {
     setText(e.target.value);
   }
 
-  function calcWordsCount() {
-    const arrText = text.split(' ').filter((word) => word !== '');
-    console.log(arrText.length);
+  function calcWordCount(words) {
+    const wordsArr = words.split(' ').filter((word) => word !== '');
+    return wordsArr.length;
+  }
+
+  useEffect(() => {
+    if (isTimeRunning && countdown > 0) {
+      const timer = setTimeout(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1);
+      }, 1000);
+      return () => clearTimeout(timer);
+    } else if (countdown === 0) {
+      endGame();
+    }
+  }, [countdown, isTimeRunning]);
+
+  function startGame() {
+    setIsTimeRunning(true);
+    setCountdown(STARTING_TIME);
+    setText('');
+  }
+
+  function endGame() {
+    setIsTimeRunning(false);
+    setWordCount(calcWordCount(text));
   }
 
   return (
     <div>
-      <h1>How fast do you type</h1>
+      <h1>How fast do you type?</h1>
 
       <textarea
+        disabled={!isTimeRunning}
         value={text}
         onChange={handleChange}
-        placeholder="Your text here"
+        placeholder="type here"
       />
-      <h4>Time remaining:???</h4>
-      <button onClick={calcWordsCount}>start</button>
-      <h2>Word count: ???</h2>
+      <h4>Time remaining: {countdown}</h4>
+      <button disabled={isTimeRunning} onClick={startGame}>
+        start
+      </button>
+      <h2>Word count: {wordCount}</h2>
     </div>
   );
 }
